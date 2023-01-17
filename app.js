f0f0b433d1236a3267fdae0579fd738627bef131
danielp67 <66970422+danielp67@users.noreply.github.com>
2023-01-17 23:16:16 +0100

    correct file

--- app.js
+++ app.js
@@ -5,21 +5,29 @@ csv = fs.readFileSync("test.csv")
 
 // Convert the data to String and
 // split it in an array
-var array = csv.toString().split("\r");
+var arraytmp = csv.toString().split("\r");
+var array =[]
+for(let i=0; i<arraytmp.length; i++)
+{
+    array.push(arraytmp[i].replace("\n",""))
+}
+
 
 // All the rows of the CSV will be
 // converted to JSON objects which
 // will be added to result in an array
+
+
 let result = [];
 
 // The array[0] contains all the
 // header columns so we store them
 // in headers array
-let headers = array[0].split(", ")
+let headers = array[0].split(";")
 
 // Since headers are separated, we
 // need to traverse remaining n-1 rows.
-for (let i = 1; i < array.length - 1; i++) {
+for (let i = 1; i < array.length; i++) {
     let obj = {}
 
 // Create an empty object to later add
@@ -44,32 +52,44 @@ for (let i = 1; i < array.length - 1; i++) {
     for (let ch of str) {
         if (ch === '"' && flag === 0) {
             flag = 1
+
         }
         else if (ch === '"' && flag == 1) flag = 0
-        if (ch === ', ' && flag === 0) ch = '|'
+        if (ch === ';' && flag === 0) ch = '|'
         if (ch !== '"') s += ch
+
+
     }
 
 // Split the string using pipe delimiter |
 // and store the values in a properties array
-    let properties = s.split("|")
+    let propertiestmp = s.split("|")
+
+    let properties = []
+    for (let a=0; a<propertiestmp.length;a++)
+    {
+        let parse = propertiestmp[a]
+        if(!isNaN(propertiestmp[a]))
+        {
+            parse = parseInt(propertiestmp[a])
+        }
+        properties.push(parse)
+    }
 
 // For each header, if the value contains
 // multiple comma separated data, then we
 // store it in the form of array otherwise
 // directly the value is stored
-    for (let j in headers) {
-        if (properties[j].includes(", ")) {
-            obj[headers[j]] = properties[j]
-                .split(", ").map(item => item.trim())
-        }
-        else obj[headers[j]] = properties[j]
-    }
+            for (let j in headers) {
+                obj[headers[j]] = properties[j]
+            }
+
 
 // Add the generated object to our
 // result array
     result.push(obj)
 }
+console.log(result)
 
 // Convert the resultant array to json and
 // generate the JSON output file.
